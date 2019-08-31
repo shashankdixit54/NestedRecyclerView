0860f762c1e9b3e4d13d41b031ececc2b7de67b7
Shashank <shashankdixit54@gmail.com>
2019-08-31 13:31:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/nestedrecyclerview/MainActivity.java
@@ -0,0 +1,69 @@
+package com.example.nestedrecyclerview;
+
+import android.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActionBar toolbar;
+
+
+    ArrayList<SectionDataModel> allSampleData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = getActionBar();
+
+        allSampleData = new ArrayList<SectionDataModel>();
+
+        if (toolbar != null) {
+            toolbar.setTitle("Nested Recycler View");
+
+        }
+
+
+        createDummyData();
+
+
+        RecyclerView my_recycler_view = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        my_recycler_view.setHasFixedSize(true);
+
+        RecyclerViewDataAdapter adapter = new RecyclerViewDataAdapter(this, allSampleData);
+
+        my_recycler_view.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        my_recycler_view.setAdapter(adapter);
+
+
+    }
+
+    public void createDummyData() {
+        for (int i = 1; i <= 54; i++) {
+
+            SectionDataModel dm = new SectionDataModel();
+
+            dm.setHeaderTitle("Week " + i);
+
+            ArrayList<SingleItemModel> singleItem = new ArrayList<SingleItemModel>();
+            for (int j = 0; j <= 5; j++) {
+                singleItem.add(new SingleItemModel("Village " + j, "URL " + j));
+            }
+
+            dm.setAllItemsInSection(singleItem);
+
+            allSampleData.add(dm);
+
+        }
+    }
+}
+
---
+++ app/src/main/java/com/example/nestedrecyclerview/RecyclerViewDataAdapter.java
@@ -0,0 +1,88 @@
+package com.example.nestedrecyclerview;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class RecyclerViewDataAdapter extends RecyclerView.Adapter<RecyclerViewDataAdapter.ItemRowHolder> {
+
+    private ArrayList<SectionDataModel> dataList;
+    private Context mContext;
+
+    public RecyclerViewDataAdapter(Context context, ArrayList<SectionDataModel> dataList) {
+        this.dataList = dataList;
+        this.mContext = context;
+    }
+
+    @Override
+    public ItemRowHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.list_item, null);
+        ItemRowHolder mh = new ItemRowHolder(v,viewGroup.getWidth());
+
+        return mh;
+    }
+
+    @Override
+    public void onBindViewHolder(ItemRowHolder itemRowHolder, int i) {
+
+        final String sectionName = dataList.get(i).getHeaderTitle();
+
+        ArrayList singleSectionItems = dataList.get(i).getAllItemsInSection();
+
+        itemRowHolder.itemTitle.setText(sectionName);
+
+        SectionListDataAdapter itemListDataAdapter = new SectionListDataAdapter(mContext, singleSectionItems);
+
+        itemRowHolder.recycler_view_list.setHasFixedSize(true);
+        itemRowHolder.recycler_view_list.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
+        itemRowHolder.recycler_view_list.setAdapter(itemListDataAdapter);
+
+
+
+       /* Glide.with(mContext)
+                .load(feedItem.getImageURL())
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .centerCrop()
+                .error(R.drawable.bg)
+                .into(feedListRowHolder.thumbView);*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return (null != dataList ? dataList.size() : 0);
+    }
+
+    public class ItemRowHolder extends RecyclerView.ViewHolder {
+
+        protected TextView itemTitle;
+
+        protected RecyclerView recycler_view_list;
+
+
+        protected LinearLayout linLayout;
+
+
+        public ItemRowHolder(View view, int width) {
+            super(view);
+
+            this.linLayout = (LinearLayout)view.findViewById(R.id.listItemLinLayout);
+            this.itemTitle = (TextView) view.findViewById(R.id.itemTitle);
+            this.recycler_view_list = (RecyclerView) view.findViewById(R.id.recycler_view_list);
+            this.recycler_view_list.getLayoutParams().width = width/4;
+
+
+
+        }
+
+    }
+
+}
